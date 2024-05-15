756f2e7c137aa381f0550f4c3a7d8bae51d6bebb
Sanghamitra Dash <101447128+sanghamitradash@users.noreply.github.com>
2024-05-15 17:35:07 +0530

    Jpa-hibernate

--- 09-spring-boot-jpa-advanced-mappings/00-jpa-advanced-mappings-database-scripts/hb-03-one-to-many/create-db.sql
+++ 09-spring-boot-jpa-advanced-mappings/00-jpa-advanced-mappings-database-scripts/hb-03-one-to-many/create-db.sql
@@ -1,54 +1,39 @@
-DROP SCHEMA IF EXISTS `hb-03-one-to-many`;
+CREATE SCHEMA "hb-03-one-to-many";
 
-CREATE SCHEMA `hb-03-one-to-many`;
+SET search_path TO "hb-03-one-to-many";
 
-use `hb-03-one-to-many`;
+DROP TABLE IF EXISTS "instructor_detail" CASCADE;
+DROP TABLE IF EXISTS "instructor" CASCADE;
+DROP TABLE IF EXISTS "course" CASCADE;
 
-SET FOREIGN_KEY_CHECKS = 0;
+CREATE TABLE "instructor_detail" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "youtube_channel" varchar(128) DEFAULT NULL,
+  "hobby" varchar(45) DEFAULT NULL
+);
 
-DROP TABLE IF EXISTS `instructor_detail`;
+CREATE TABLE "instructor" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "first_name" varchar(45) DEFAULT NULL,
+  "last_name" varchar(45) DEFAULT NULL,
+  "email" varchar(45) DEFAULT NULL,
+  "instructor_detail_id" int DEFAULT NULL,
+  CONSTRAINT "FK_DETAIL" FOREIGN KEY ("instructor_detail_id") 
+  REFERENCES "instructor_detail" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+);
 
-CREATE TABLE `instructor_detail` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `youtube_channel` varchar(128) DEFAULT NULL,
-  `hobby` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-
-
-DROP TABLE IF EXISTS `instructor`;
-
-CREATE TABLE `instructor` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `instructor_detail_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_DETAIL_idx` (`instructor_detail_id`),
-  CONSTRAINT `FK_DETAIL` FOREIGN KEY (`instructor_detail_id`) 
-  REFERENCES `instructor_detail` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS `course`;
-
-CREATE TABLE `course` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(128) DEFAULT NULL,
-  `instructor_id` int DEFAULT NULL,
+CREATE TABLE "course" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "title" varchar(128) DEFAULT NULL,
+  "instructor_id" int DEFAULT NULL,
+  UNIQUE ("title"),
   
-  PRIMARY KEY (`id`),
-  
-  UNIQUE KEY `TITLE_UNIQUE` (`title`),
-  
-  KEY `FK_INSTRUCTOR_idx` (`instructor_id`),
-  
-  CONSTRAINT `FK_INSTRUCTOR` 
-  FOREIGN KEY (`instructor_id`) 
-  REFERENCES `instructor` (`id`) 
+  CONSTRAINT "FK_INSTRUCTOR"
+  FOREIGN KEY ("instructor_id") 
+  REFERENCES "instructor" ("id") 
   
   ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+);
 
 
-SET FOREIGN_KEY_CHECKS = 1;
+-- SET FOREIGN_KEY_CHECKS = 1;
---
+++ 09-spring-boot-jpa-advanced-mappings/07-jpa-one-to-many-create-instructor-with-courses/src/main/java/com/example/cruddemo_sec9_udemy/CruddemoSec9UdemyApplication.java
@@ -0,0 +1,155 @@
+package com.example.cruddemo_sec9_udemy;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.example.cruddemo_sec9_udemy.dao.AppDAO;
+import com.example.cruddemo_sec9_udemy.entity.Course;
+import com.example.cruddemo_sec9_udemy.entity.Instructor;
+import com.example.cruddemo_sec9_udemy.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoSec9UdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoSec9UdemyApplication.class, args);
+	}
+
+	//create command line runner
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO){
+		//java LAMBDA expression
+		return runner -> {
+			//createInstructor(appDAO);
+
+			//findInstructor(appDAO);
+
+			//deleteInstructor(appDAO);
+
+			//findInstructorDetail(appDAO);
+
+			//deleteInstructorDetail(appDAO);
+			
+			createInstructorWithCourses(appDAO);
+
+		};
+	}
+
+	private void createInstructorWithCourses(AppDAO appDAO) {
+		//create the instructor
+		Instructor tempInstructor = new Instructor("Susan","Public","susan@gmail.com");
+
+		//create instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail(
+				"https://www.youtube.com",
+				"Video Games");
+
+
+		//associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		//create some courses
+		Course tempCourse1 = new Course("Air Guitar - The Ultimate Guide");
+		Course tempCourse2 = new Course("The Pinball Masterclass");
+
+		//add courses to instructor
+		tempInstructor.add(tempCourse1);
+		tempInstructor.add(tempCourse2);
+
+		//save the instructor
+		//
+		//NOTE: this will also save the courses
+		//because of CascadeType.PERSIST
+		//
+		System.out.println("Saving instructor: " +tempInstructor);
+		System.out.println("The courses: " +tempInstructor.getCourses());
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!");
+	}
+
+	private void deleteInstructorDetail(AppDAO appDAO) {
+
+		int theId = 4;
+		System.out.println("Deleting instructor detail id: "+ theId);
+
+		appDAO.deleteInstructorDetailById(theId);
+
+		System.out.println("Done!!");
+	}
+
+	private void findInstructorDetail(AppDAO appDAO) {
+
+		//get the instructor detail object
+		int theId = 2;
+		InstructorDetail tempInstructorDetail = appDAO.findInstructorDetailById(theId);
+
+		//print the instructor detail
+		System.out.println("tempInstructorDetail: "+tempInstructorDetail);
+
+		//print the associated instructor
+		System.out.println("the associated instructor: "+tempInstructorDetail.getInstructor());
+
+		System.out.println("Done!!");
+
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Deleting the instructor id: "+theId);
+
+		appDAO.deleteInstructorById(theId);
+
+		System.out.println("Done!!");
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+
+		int theId = 2;
+		System.out.println("Finding instructor id: "+theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: "+ tempInstructor);
+		System.out.println("the associated instructorDetail only: "+ tempInstructor.getInstructorDetail());
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+
+		/*
+		//create the instructor
+		Instructor tempInstructor = new Instructor("Chad","Darby","chad@gmail.com");
+
+		//create instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail(
+				"https://www.youtube.com/youtube",
+				"love to code");
+
+		 */
+
+		//create the instructor
+		Instructor tempInstructor = new Instructor("Madhu","Patel","madhu@gmail.com");
+
+		//create instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail(
+				"https://www.youtube.com/youtube",
+				"love to dance");
+
+
+		//associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		//save the instructor
+		//
+		//NOTE: this will ALSO save the details object
+		//because of cascadeType.ALL
+		//
+		System.out.println("Saving instructor: "+tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!!");
+	}
+
+}
